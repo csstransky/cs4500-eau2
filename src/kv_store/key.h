733c5d73006c4a26ee8e18794527cc5119f2a528
bazooka <csstransky@gmail.com>
2020-03-18 16:02:55 -0400

    Key class complete

--- src/kv_store/key.h
+++ src/kv_store/key.h
@@ -3,12 +3,18 @@
 #include "../helpers/string.h"
 
 class Key : public Object {
+    public:
     String* key_;
     size_t node_index_;
 
     Key(String* key, size_t node_index) {
         key_ = key->clone();
         node_index_ = node_index;
+    }
+
+    Key(Key& from) : Object(from) {
+        key_ = from.key_->clone();
+        node_index_ = from.node_index_;
     }
 
     ~Key() {
@@ -24,12 +30,44 @@ class Key : public Object {
     }
 
     /** Subclasses should redefine */
-    bool equals(Object  * other) {
-        // TODO
+    bool equals(Object* other) {
+        if (other == this) return true;
+        Key* other_key = dynamic_cast<Key*>(other);
+        return other_key != nullptr 
+            && this->key_->equals(other_key->key_)
+            && this->node_index_ == other_key->node_index_;
     }
 
     /** Return a copy of the object; nullptr is considered an error */
     Object* clone() {
-        // TODO
+        return new Key(*this);
+    }
+
+    size_t serial_len() {
+        // Includes the seial length, size of the key string, and size of node index
+        return sizeof(size_t) + key_->serial_len() + sizeof(size_t);
+    }
+
+    char* serialize() {
+        size_t serial_size = serial_len();
+        Serializer serializer(serial_size);
+        serializer.serialize_size_t(serial_size);
+        serializer.serialize_object(key_);
+        serializer.serialize_size_t(node_index_);
+        return serializer.get_serial();
+    }
+
+    static Key* deserialize(char* serial) {
+        Deserializer deserializer(serial);
+        return deserialize(deserializer);
+    }
+
+    static Key* deserialize(Deserializer& deserializer) {
+        deserializer.deserialize_size_t();
+        String* key_string = String::deserialize(deserializer);
+        size_t node_index = deserializer.deserialize_size_t();
+        Key* new_key = new Key(key_string, node_index);
+        delete key_string;
+        return new_key;
     }
 };
