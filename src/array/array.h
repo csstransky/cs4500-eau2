7edc0487e24e1234fcbf4dcceee004877a6857af
bazooka <csstransky@gmail.com>
2020-03-20 00:41:53 -0400

    Added ColumnArray (untested)

--- src/array/array.h
+++ src/array/array.h
@@ -930,8 +930,8 @@ public:
 };
 
 /**
- * An Array class to which Strings can be added to and removed from.
- * author: chasebish */
+ * An Array class to which Keys can be added to and removed from.
+ * author: kaylindevchand & csstransky */
 class KeyArray : public ObjectArray {
 public:
   /** CONSTRUCTORS & DESTRUCTORS **/
@@ -957,35 +957,35 @@ public:
     return new KeyArray(this);
   }
 
-  /* Adds an StringArray to existing contents */
+  /* Adds an KeyArray to existing contents */
   void concat(KeyArray* const arr) {
     ObjectArray::concat(arr);
   }
 
-  /* Gets a String at the given index */
+  /* Gets a Key at the given index */
   /* Throws an error if not found or out of range or no elements in array*/
   Key* get(size_t index) {
     return dynamic_cast<Key*>(ObjectArray::get(index));
   }
 
-  /* Removes the last String of the Array, returns the removed String */
+  /* Removes the last Key of the Array, returns the removed Key */
   /* Throws an error if not found or out of range or no elements in array*/
   Key* pop() {
     return dynamic_cast<Key*>(ObjectArray::pop());
   }
 
-  /* Adds an String to the end of the Array, returns the new length */
+  /* Adds an Key to the end of the Array, returns the new length */
   size_t push(Key* const to_add) {
     return ObjectArray::push(to_add);
   }
 
-  /* Removes a String at the given index, returns removed String */
+  /* Removes a Key at the given index, returns removed Key */
   /* Throws an error if not found or out of range or no elements in array*/
   Key* remove(size_t index) {
     return dynamic_cast<Key*>(ObjectArray::remove(index));
   }
 
-  /* Replaces a String at the given index with the given String, returns the replaced String */
+  /* Replaces a Key at the given index with the given Key, returns the replaced Key */
   /* Throws an error if not found or out of range or no elements in array*/
   Key* replace(size_t index, Key* const to_add) {
     return dynamic_cast<Key*>(ObjectArray::replace(index, to_add));
@@ -1008,5 +1008,88 @@ public:
         delete new_object;
       }
       return new_array;
+  }
+};
+
+/**
+ * An Array class to which Columns can be added to and removed from.
+ * author: kaylindevchand & csstransky */
+class ColumnArray : public ObjectArray {
+public:
+  /** CONSTRUCTORS & DESTRUCTORS **/
+
+  /* Creates an Array of desired size */
+  ColumnArray(const size_t size) : ObjectArray(size) {
+    
+  }
+
+  /* Copies the contents of an already existing Array */
+  ColumnArray(ColumnArray* arr) : ObjectArray(arr) {
+    
+  }
+
+  /* Clears Array from memory */
+  ~ColumnArray() {
+
+  }
+
+  /** ARRAY METHODS **/
+
+  ColumnArray* clone() {
+    return new ColumnArray(this);
+  }
+
+  /* Adds an ColumnArray to existing contents */
+  void concat(ColumnArray* const arr) {
+    ObjectArray::concat(arr);
+  }
+
+  /* Gets a Column at the given index */
+  /* Throws an error if not found or out of range or no elements in array*/
+  Column* get(size_t index) {
+    return dynamic_cast<Column*>(ObjectArray::get(index));
+  }
+
+  /* Removes the last Column of the Array, returns the removed Column */
+  /* Throws an error if not found or out of range or no elements in array*/
+  Column* pop() {
+    return dynamic_cast<Column*>(ObjectArray::pop());
+  }
+
+  /* Adds an Column to the end of the Array, returns the new length */
+  size_t push(Column* const to_add) {
+    return ObjectArray::push(to_add);
+  }
+
+  /* Removes a Column at the given index, returns removed Column */
+  /* Throws an error if not found or out of range or no elements in array*/
+  Column* remove(size_t index) {
+    return dynamic_cast<Column*>(ObjectArray::remove(index));
+  }
+
+  /* Replaces a Column at the given index with the given Column, returns the replaced Column */
+  /* Throws an error if not found or out of range or no elements in array*/
+  Column* replace(size_t index, Column* const to_add) {
+    return dynamic_cast<Column*>(ObjectArray::replace(index, to_add));
+  }
+
+  static ColumnArray* deserialize(char* serial) {
+    Deserializer deserializer(serial);
+    return deserialize(deserializer);
+  }
+
+  static ColumnArray* deserialize(Deserializer& deserializer) {
+    // Don't need serial size, so we skip it
+    deserializer.deserialize_size_t();
+    size_t size = deserializer.deserialize_size_t();
+    size_t count = deserializer.deserialize_size_t();
+    ColumnArray* new_array = new ColumnArray(size);
+    for (size_t ii = 0; ii < count; ii++) {
+      // TODO: We'll have to add KV stores somehow in the future, nullptr won't cut it
+      Column* new_object = Column::deserialize(deserializer, nullptr);
+      new_array->push(new_object);
+      delete new_object;
+    }
+    return new_array;
   }
 };
