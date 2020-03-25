81ce4019a6739019710f4338dcfc50ee53a50530
kaylindevchand <kaydevchand@gmail.com>
2020-03-25 10:42:02 -0400

    fix valgrind

--- tests/test_networking.cpp
+++ tests/test_networking.cpp
@@ -5,21 +5,21 @@
 
 void test_connection() {
     int cpid;
-    String server_ip("127.0.0.1");
-    String client_ip("127.0.0.2");
+    String* server_ip = new String("127.0.0.1");
+    String* client_ip = new String("127.0.0.2");
 
     // Fork to create another process
     if ((cpid = fork())) {
         // In parent process
 
         // Start server
-        RServer* server = new RServer(server_ip.c_str()); 
+        RServer* server = new RServer(server_ip->c_str()); 
         server->run_server(2);
 
         // check for registered client
         assert(server->client_sockets_->length() == 1);
         assert(server->connected_client_ips_->length() == 1);
-        assert(server->connected_client_ips_->get(0)->equals(&client_ip));
+        assert(server->connected_client_ips_->get(0)->equals(client_ip));
         assert(server->node_indexes_->length() == 1);
         assert(server->node_indexes_->get(0) == 0);
         
@@ -29,6 +29,8 @@ void test_connection() {
         server->run_server(0.5);
         server->shutdown();
         delete server;
+        delete client_ip;
+        delete server_ip;
     } else {
         // In child process
 
@@ -36,26 +38,26 @@ void test_connection() {
         sleep(0.5);
 
         // start node
-        Node* node = new Node(client_ip.c_str(), server_ip.c_str());
+        Node* node = new Node(client_ip->c_str(), server_ip->c_str());
         node->connect_to_server(0);
-        node->run_server(2);
+        node->run_server(5);
 
         // check for list of nodes
         assert(node->other_nodes_->length() == 1);
-        assert(node->other_nodes_->get(0)->equals(&client_ip));
+        assert(node->other_nodes_->get(0)->equals(client_ip));
         assert(node->other_node_indexes_->length() == 1);
         assert(node->other_node_indexes_->get(0) == 0);
 
         node->shutdown();
         delete node;
+        delete server_ip;
+        delete client_ip;
 
         // exit
         exit(0);
     }
 
     printf("Networking registration test passed!\n");
-
-
 }
 
 int main(int argc, char** argv) {
