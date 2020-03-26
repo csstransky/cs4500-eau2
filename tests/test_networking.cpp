399166e221989ac07b5701c4b6d8764971e244d9
kaylindevchand <kaydevchand@gmail.com>
2020-03-26 10:59:26 -0400

    multiple nodes test

--- tests/test_networking.cpp
+++ tests/test_networking.cpp
@@ -113,9 +113,80 @@ void test_kill() {
     printf("Networking kill message test passed!\n");
 }
 
+void test_multiple_nodes() {
+    int cpid[3];
+    String* server_ip = new String("127.0.0.1");
+    String** client_ips = new String*[3];
+    client_ips[0] = new String("127.0.0.2");
+    client_ips[1] = new String("127.0.0.3");
+    client_ips[2] = new String("127.0.0.4");
+
+    // In parent process
+
+    // Start server
+    RServer* server = new RServer(server_ip->c_str()); 
+
+    for (int i = 0; i < 3; i++) {
+        if ((cpid[i] = fork())) {
+            // do nothing now
+        } else {
+            // In child process
+
+            // start node
+            Node* node = new Node(client_ips[i]->c_str(), server_ip->c_str());
+            node->connect_to_server(0);
+            node->run_server(2);
+
+            // check for list of nodes
+            assert(node->other_nodes_->length() == 3);
+            assert(node->other_node_indexes_->length() == 3);
+            printf("Node %d passed asserts\n", i);
+
+            node->run_server(-1);
+            delete node;
+            delete server_ip;
+            for (int i = 0; i < 3; i++) {
+                delete client_ips[i];
+            }
+            delete[] client_ips;
+            delete server;
+
+            // exit
+            exit(0);
+        }
+    }
+    server->run_server(3);
+
+
+    // check for registered client
+    assert(server->client_sockets_->length() == 3);
+    assert(server->connected_client_ips_->length() == 3);
+    assert(server->node_indexes_->length() == 3);
+    printf("Parent passed asserts\n");
+    server->run_server(0.5);
+    server->shutdown();
+    
+    // wait for children to finish
+    for (int i = 0; i < 3; i++) {
+        int st;
+        waitpid(cpid[i], &st, 0);
+    }
+
+    delete server;
+    for (int i = 0; i < 3; i++) {
+        delete client_ips[i];
+    }
+    delete[] client_ips;
+    delete server_ip;
+
+    printf("Networking multiple nodes test passed!\n");
+}
+
+
 int main(int argc, char** argv) {
     test_registration();
     test_kill();
+    test_multiple_nodes();
     printf("All networking tests pass!\n");
     return 0;
 }
