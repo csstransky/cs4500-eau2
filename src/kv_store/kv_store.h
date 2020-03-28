7391a749b40be6c49ac8c1eec80225f840600d03
bazooka <csstransky@gmail.com>
2020-03-28 18:10:09 -0400

    Refactored a lot of code to make more legible

--- src/kv_store/kv_store.h
+++ src/kv_store/kv_store.h
@@ -6,10 +6,17 @@
 #include "key.h"
 #include "../networks/node.h"
 
+// This is a pseudo value that's used to recognize when an expected value in the get_queue_ is 
+// actually meant for the local kv_store, and not a remote kv_store.
+// NOTE: file descriptor 0 is stdin so this shouldn't be a socket descriptor, choosing other values
+// for this variable can cause unexpected behavior (it's possible for a remote kv_store to have a 
+// node_index of 6, so making this variable 6 will cause all values to be sent to the remote).
+const int LOCAL_SOCKET_DESCRIPTOR = 0;
+
 class KV_Store : public Node {
     public:
     SOMap* kv_map_; // String* -> Serializer* 
-    SIMap* get_queue_;
+    SIAMap* get_queue_;
     size_t local_node_index_;
     std::mutex kv_map_mutex_;
     std::mutex get_queue_mutex_;
@@ -18,13 +25,13 @@ class KV_Store : public Node {
     KV_Store(const char* client_ip_address, const char* server_ip_address, size_t local_node_index) 
         : Node(client_ip_address, server_ip_address) {
         kv_map_ = new SOMap();
-        get_queue_ = new SIMap();
+        get_queue_ = new SIAMap();
         local_node_index_ = local_node_index;
     }
 
     KV_Store(size_t local_node_index) : Node() {
         kv_map_ = new SOMap();
-        get_queue_ = new SIMap();
+        get_queue_ = new SIAMap();
         local_node_index_ = local_node_index;
     }
 
@@ -33,20 +40,34 @@ class KV_Store : public Node {
         delete get_queue_;
     }
 
-    void put_map_(String* key_name, Serializer* value) {
-        std::unique_lock<std::mutex> lock(kv_map_mutex_);
-        kv_map_->put(key_name, value);
-        lock.unlock();
+    void distribute_value_(IntArray* sockets, Serializer* value) {
+        for (int i = 0; i < sockets->length(); i++) {
+            int socket = sockets->get(i);
+            if (socket > LOCAL_SOCKET_DESCRIPTOR) {
+                // TODO: Target IPs will be removed in the future
+                String no_ip("NO TARGET IP");
+                Value value_message(my_ip_, &no_ip, value);
+                send_message(socket, &value_message);
+            } else if (socket == LOCAL_SOCKET_DESCRIPTOR) {
+                cv_.notify_one();
+            }
+        }
+    }
 
-        std::unique_lock<std::mutex> lck(get_queue_mutex_);
-        int socket = get_queue_->remove(key_name);
-        lck.unlock();
-        if (socket > 0) {
-            // TODO: lost target ip
-            Value value_message(my_ip_, my_ip_, value);
-            send_message(socket, &value_message);
-        } else if (socket == 0) {
-            cv_.notify_one();
+    // Puts the key value pair into the map, and also sents the new key value pair to anyone
+    // waiting in the queue
+    void put_map_(String* key_name, Serializer* value) {
+        std::unique_lock<std::mutex> kv_lock(kv_map_mutex_);
+        kv_map_->put(key_name, value);
+        kv_lock.unlock();
+
+        std::unique_lock<std::mutex> get_lock(get_queue_mutex_);
+        IntArray* sockets = dynamic_cast<IntArray*>(get_queue_->remove(key_name));
+        get_lock.unlock();
+        
+        if (sockets) {
+            distribute_value_(sockets, value);
+            delete sockets;
         }
     }
 
@@ -56,7 +77,7 @@ class KV_Store : public Node {
 
     void put_get_queue_(String* key_name, int socket) {
         std::unique_lock<std::mutex> lock(get_queue_mutex_);
-        get_queue_->put(key_name, socket);
+        put_socket_into_queue_(key_name, socket);
     }
 
     void put(Key* key, Object* value) {
@@ -77,6 +98,14 @@ class KV_Store : public Node {
         return local_node_index_;
     }
 
+    // NOTE: message should be either a Get or WaitAndGet
+    char* send_message_and_receive_serial_(Message& message) {
+        Value* value_message = dynamic_cast<Value*>(send_message_to_node_wait(&message));
+        char* serial = value_message->get_serial();
+        delete value_message;
+        return serial;
+    }
+
     // Returns a new char array, make sure to delete it later
     char* get_value_serial(Key* key) {
         if (key->get_node_index() == local_node_index_) {
@@ -86,12 +115,28 @@ class KV_Store : public Node {
         else {
             int index = other_node_indexes_->index_of(key->get_node_index());
             Get message(my_ip_, other_nodes_->get(index), key->get_key());
-            Value* value_message = dynamic_cast<Value*>(send_message_to_node_wait(&message));
-            char* serial = value_message->get_serial();
-            delete value_message;
-            return serial;
+            return send_message_and_receive_serial_(message);
         }
 
+    }
+
+    void put_socket_into_queue_(String* key_name, int socket_descriptor) {
+        IntArray* sockets = get_queue_->get(key_name);
+        if (sockets) {
+            sockets->push(socket_descriptor);
+        } else {
+            IntArray int_array(1);
+            int_array.push(socket_descriptor);
+            get_queue_->put(key_name, &int_array);
+        }
+    }
+
+    Serializer* wait_for_local_map_value_(Key* key) {
+        put_socket_into_queue_(key->get_key(), LOCAL_SOCKET_DESCRIPTOR);
+        std::unique_lock<std::mutex> lock(kv_map_mutex_);
+        cv_.wait(lock);
+        lock.unlock();
+        return get_map_(key->get_key());
     }
 
     char* wait_get_value_serial(Key* key) {
@@ -99,23 +144,14 @@ class KV_Store : public Node {
             Serializer* map_serial = get_map_(key->get_key());
 
             if (!map_serial) {
-                // file descriptor 0 is stdin so this shouldn't be a socket descriptor
-                get_queue_->put(key->get_key(), 0);
-                std::unique_lock<std::mutex> lock(kv_map_mutex_);
-                cv_.wait(lock);
-                lock.unlock();
-                map_serial = get_map_(key->get_key());
-            }
-
+                map_serial = wait_for_local_map_value_(key); 
+            } 
             return map_serial->get_serial();
         }
         else {
             int index = other_node_indexes_->index_of(key->get_node_index());
-            WaitGet message(my_ip_, other_nodes_->get(index), key->get_key());
-            Value* value_message = dynamic_cast<Value*>(send_message_to_node_wait(&message));
-            char* serial = value_message->get_serial();
-            delete value_message;
-            return serial;
+            WaitAndGet message(my_ip_, other_nodes_->get(index), key->get_key());
+            return send_message_and_receive_serial_(message);
         }
 
     }
@@ -153,6 +189,7 @@ class KV_Store : public Node {
     }
 
     // TODO: Cristian I think you wanted a different algorithm but I forget what is was
+    // Answer: 10 columns in dataframe, 4 kv_stores: 0 1 2 3 0 1 2 3 0 1
     size_t get_random_node_index() {
         int num_of_nodes = get_num_other_nodes();
         if (num_of_nodes == -1) {
@@ -186,8 +223,8 @@ class KV_Store : public Node {
                 send_message(client_sockets_->get(client), &value_message);
                 break;
             }
-            case MsgKind::WaitGet: {
-                WaitGet* get_message = dynamic_cast<WaitGet*>(message);
+            case MsgKind::WaitAndGet: {
+                WaitAndGet* get_message = dynamic_cast<WaitAndGet*>(message);
                 Serializer* value = get_map_(get_message->get_key_name());
                 if (value) {
                     Value value_message(my_ip_, get_message->get_sender(), value);
@@ -199,7 +236,7 @@ class KV_Store : public Node {
             }   
             default:
                 // Nobody inherits from kv store so it has to handle the message
-                assert(0);
+                return 0;
         }
         return 1;
     }
