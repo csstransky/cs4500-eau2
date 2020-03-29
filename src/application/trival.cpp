d9233bb09d4566560d76a8ac000e777114a59db3
kaylindevchand <kaydevchand@gmail.com>
2020-03-29 00:16:55 -0400

    Have trivial read from command line

--- src/application/trival.cpp
+++ src/application/trival.cpp
@@ -1,10 +1,37 @@
 #include "application.h"
 
+const char* DEFAULT_CLIENT_IP = "127.0.0.2";
+const char* DEFAULT_SERVER_IP = "127.0.0.1";
+
+// TODO: Super rigid input command getter, but it works and we don't need anything too nice
+const char* get_input_client_ip_address(int argc, char const *argv[]) {
+    if (argc < 2 || strcmp(argv[1], "-ip") != 0) {
+        printf("If you wish to choose an IP for the client, use:\n");
+        printf("./client -ip <IP address>\n\n");
+        return DEFAULT_CLIENT_IP;
+    }
+    else {
+        return argv[2];
+    }
+}
+
+// TODO: Super rigid input command getter, but it works and we don't need anything too nice
+const char* get_input_server_ip_address(int argc, char const *argv[]) {
+    if (argc < 4 || strcmp(argv[3], "-s") != 0) {
+        printf("If you wish to choose an IP for the client AND the server, use:\n");
+        printf("./client -ip <IP address> -s <Server IP address>\n\n");
+        return DEFAULT_SERVER_IP;
+    }
+    else {
+        return argv[4];
+    }
+}
+
 // Main test for Milestone 2:
 // http://janvitek.org/events/NEU/4500/s20/projects2.html
 class Trivial : public Application {
  public:
-  Trivial(size_t idx) : Application(idx) { }
+  Trivial(size_t idx, const char* my_ip, const char* server_ip) : Application(idx, my_ip, server_ip) { }
   void run_() {
     size_t SZ = 1000*1000;
     float* vals = new float[SZ];
@@ -22,7 +49,9 @@ class Trivial : public Application {
   }
 };
 
-int main(int argc, char** argv) {
-  Trivial t_app(0);
+int main(int argc, const char** argv) {
+  const char* client_ip_address = get_input_client_ip_address(argc, argv);
+  const char* server_ip_address = get_input_server_ip_address(argc, argv);
+  Trivial t_app(0, client_ip_address, server_ip_address);
   t_app.run_();
 }
