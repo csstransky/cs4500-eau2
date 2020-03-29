85af26b706bb7a974ed837c5f40b8083903afaeb
kaylindevchand <kaydevchand@gmail.com>
2020-03-28 23:55:25 -0400

    Add network stuff to application

--- src/application/application.h
+++ src/application/application.h
@@ -5,8 +5,12 @@ class Application : public Object {
     KD_Store kd_;
     size_t node_index_;
 
-    Application(size_t node_index) : kd_(node_index) {
+    Application(size_t node_index, const char* my_ip, const char* server_ip) : kd_(node_index, my_ip, server_ip) {
         node_index_ = node_index;
+    }
+
+    ~Application() {
+        kd_.application_complete();
     }
 
     virtual void run_() = 0;
